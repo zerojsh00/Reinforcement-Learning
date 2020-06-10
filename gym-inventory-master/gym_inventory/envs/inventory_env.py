b4e5692de0035cd757b7b4b47d0e083d97cc1a72
Sanghyeon Jung (Simon) <65492843+zerojsh00@users.noreply.github.com>
2020-06-11 00:40:59 +0900

    error fixed

--- gym-inventory-master/gym_inventory/envs/inventory_env.py
+++ gym-inventory-master/gym_inventory/envs/inventory_env.py
@@ -17,18 +17,19 @@ class InventoryEnv(gym.Env, utils.EzPickle):
         The goal of the inventory manager is to manage the inventory so as to maximize the present monetary value of the expected total future income.
     """
 
-    def __init__(self, n=100, k=8, c=2, h=6, p=9, lam=8):
+    def __init__(self, n=100, k=100, c=6, h=2, p=10, penalty=100, lam=70):
         self.n = n
         self.action_space = spaces.Discrete(n) # the number of items ordered in the evening of day t
         self.observation_space = spaces.Discrete(n) # the inventory size
         self.max = n
-        self.state = 0 # 최초 보유 재고량
+        self.state = 99 # 최초 보유 재고량
         self.k = k # a fixed entry cost k of ordering nonzero items
         self.c = c # a fixed cost c
         self.h = h # a cost of holding an inventory of size x > 0
         self.p = p
         self.lam = lam
         self.done = False
+        self.penalty = penalty
 
         # Set seed
         self.seed()
@@ -41,15 +42,21 @@ class InventoryEnv(gym.Env, utils.EzPickle):
 
     def transition(self, x, a, d):
         m = self.max
-        return max(min(x + a, m) - d, 0)
+        if min(x + a, m) - d < 0 :
+            lost_sale = min(x + a, m) - d
+        else :
+            lost_sale = 0
+        next_inventory_level = max(min(x + a, m) - d, 0)
+        return next_inventory_level, lost_sale
 
-    def reward(self, x, a, y):
+    def reward(self, x, a, y, lost_sale):
         k = self.k
         m = self.max
         c = self.c
         h = self.h
         p = self.p
-        r = -k * (a > 0) - c * max(min(x + a, m) - x, 0) - h * x + p * max(min(x + a, m) - y, 0)
+        penalty = self.penalty
+        r = -k * (a > 0) - c * max(min(x + a, m) - x, 0) - h * x + p * max(min(x + a, m) - y, 0) + penalty * lost_sale
         return r
 
     def seed(self, seed=None):
@@ -61,13 +68,14 @@ class InventoryEnv(gym.Env, utils.EzPickle):
         done = self.done
         obs = self.state
         demand = self.demand()
-        obs2 = self.transition(obs, action, demand)
+        obs2, lost_sale = self.transition(obs, action, demand)
         self.state = obs2
-        reward = self.reward(obs, action, obs2)
+        reward = self.reward(obs, action, obs2, lost_sale)
         # done = 0
         # if reward > 0:
         #     done = True
-        return obs2, reward, done, {}
+        return obs2, reward, done, demand
 
     def reset(self):
+        self.state = self.action_space.sample()
         return self.state
